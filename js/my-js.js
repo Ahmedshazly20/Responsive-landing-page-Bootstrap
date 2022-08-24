4b5ed2390d5ae3e53c0668933296189ba3b0a1ca
Ahmedshazly20 <Ahmed.shazly2019@gmail.com>
2022-08-24 18:25:54 +0200

    add swiper carousel

---
+++ js/my-js.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 2000,
+      },
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+      
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        768:{
+            slidesPerView: 3,
+          spaceBetween: 40
+        },
+        992:{
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+      }
+  });
